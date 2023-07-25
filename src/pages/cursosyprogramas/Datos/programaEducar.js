45fbccef94ef32c29fe31d533971b631c482945b
FlorMarleny <flor.rodriguez@tecsup.edu.pe>
2023-07-25 14:19:36 -0500

    nueva actualizacion CYP

--- src/pages/cursosyprogramas/Datos/programaEducar.js
+++ src/pages/cursosyprogramas/Datos/programaEducar.js
@@ -1,14 +1,15 @@
 export const programaEducar = {
-    id: 3,
-    nombre: "Programa Educar",
-    subprogramas: [
-      {
-        id: 1,
-        nombre: "Programa Educar",
-        imagen: "src/img/imagenCursos.jpg",
-        texto: "Fortalecer competencias digitales docentes con TIC y estrategias didácticas para motivar participación activa estudiantil.",
-        modalidad: "Síncrona",
-        gratis: true,
-      },
-    ],
-  };
+  id: 3,
+  nombre: "Programa Educar",
+  subprogramas: [
+    {
+      id: 1,
+      nombre: "Programa Educar",
+      imagen: "src/img/imagenCursos.jpg",
+      texto:
+        "Fortalecer competencias digitales docentes con TIC y estrategias didácticas para motivar participación activa estudiantil.",
+      modalidad: "Síncrona",
+      gratis: true,
+    },
+  ],
+};

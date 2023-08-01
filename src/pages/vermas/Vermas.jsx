8398dc01c3fa3f780e4ec315f2fed874a40c3f01
FlorMarleny <flor.rodriguez@tecsup.edu.pe>
2023-08-01 10:50:00 -0500

    vermas.............

--- src/pages/vermas/Vermas.jsx
+++ src/pages/vermas/Vermas.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import { useLocation } from "react-router-dom";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import './Vermas.css';
 
 const Vermas = () => {
   const location = useLocation();
@@ -23,7 +25,44 @@ const Vermas = () => {
           <p>Horas Académicas: {subprograma.horasAcademicas}</p>
           <h4>Descripción:</h4>
           <p>{subprograma.descripcion}</p>
+
+
+          <h4>Contenidos:</h4>
+          <Tabs>
+            <TabList>
+              <Tab>{subprograma.Contenido1}</Tab>
+              <Tab>{subprograma.Contenido2}</Tab>
+              <Tab>{subprograma.Contenido3}</Tab>
+              <Tab>{subprograma.Contenido4}</Tab>
+              <Tab>{subprograma.Contenido5}</Tab>
+              <Tab>{subprograma.Contenido6}</Tab>
+              <Tab>{subprograma.Contenido7}</Tab>
+            </TabList>
+
+            <TabPanel>
+              <p>{subprograma.Contenido1}</p>
+            </TabPanel>
+            <TabPanel>
+              <p>{subprograma.Contenido2}</p>
+            </TabPanel>
+            <TabPanel>
+              <p>{subprograma.Contenido3}</p>
+            </TabPanel>
+            <TabPanel>
+              <p>{subprograma.Contenido4}</p>
+            </TabPanel>
+            <TabPanel>
+              <p>{subprograma.Contenido5}</p>
+            </TabPanel>
+            <TabPanel>
+              <p>{subprograma.Contenido6}</p>
+            </TabPanel>
+            <TabPanel>
+              <p>{subprograma.Contenido7}</p>
+            </TabPanel>
+          </Tabs>
         </div>
+
         <div className="col-md-6 d-flex align-items-center justify-content-center">
           <img
             src={subprograma.imagen}
@@ -31,6 +70,7 @@ const Vermas = () => {
             className="img-fluid"
           />
         </div>
+   
       </div>
     </div>
   );
